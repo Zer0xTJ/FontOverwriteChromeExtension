b6e5d7c0119e91c8ee24661b55fe991c5fd80b5c
zer0xtj <zer0xtj.code@gmail.com>
2025-04-16 05:41:51 +0200

    add whitelist support

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,18 @@
-setInterval(() => {
-    const allElements = document.body.getElementsByTagName("*");
-    for (let i = 0; i < allElements.length; i++) {
-        const el = allElements[i];
-        if (el.childNodes.length === 1 && el.childNodes[0].nodeType === Node.TEXT_NODE && el.innerText.trim() !== "") {
-            el.style.setProperty('font-family', "'Noto Sans Arabic', sans-serif", 'important');
+const whitelist = [
+    "x.com"
+]
+
+const host = window.location.host;
+if (!whitelist.includes(host)) {
+    console.log("Not whitelisted for font replacement");
+} else {
+    setInterval(() => {
+        const allElements = document.body.getElementsByTagName("*");
+        for (let i = 0; i < allElements.length; i++) {
+            const el = allElements[i];
+            if (el.childNodes.length === 1 && el.childNodes[0].nodeType === Node.TEXT_NODE && el.innerText.trim() !== "") {
+                el.style.setProperty('font-family', "'Noto Sans Arabic', sans-serif", 'important');
+            }
         }
-    }
-}, 100);
+    }, 100);
+}
