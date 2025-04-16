f0e6b3ce0d3fe2ae2949b86d58748c5da6916ee7
zer0xtj <zer0xtj.code@gmail.com>
2025-04-16 05:36:57 +0200

    init

---
+++ scripts/content.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+    const allElements = document.body.getElementsByTagName("*");
+    for (let i = 0; i < allElements.length; i++) {
+        const el = allElements[i];
+        if (el.childNodes.length === 1 && el.childNodes[0].nodeType === Node.TEXT_NODE && el.innerText.trim() !== "") {
+            el.style.setProperty('font-family', "'Noto Sans Arabic', sans-serif", 'important');
+        }
+    }
+}, 100);
